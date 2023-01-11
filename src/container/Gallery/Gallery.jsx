f52e8e3736808edf980338ba0ee26803aae70c88
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-01-11 12:04:02 +0200

    added gallery photos

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -6,7 +6,21 @@ import { images } from '../../constants';
 
 import './Gallery.css';
 
+const galleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
+
 const Gallery = () => {
+  const scrollRef = React.useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if(direction === 'left') {
+      current.scrollleft -=300;
+    } 
+    else{
+      current.scrollleft += 300;
+    }
+  }
  
   return (
      <div className='app__gallery flex__center'>
@@ -15,6 +29,24 @@ const Gallery = () => {
         <h1 className='headtext__cormorant'>Photo Gallery</h1>
         <p className='P__opensans' style={{ color: '#AAA',marginTop: '2rem'}}>These are just some of the photos of foods that we serve here #@Mimie's restaurant.</p>
         <button type='button' className='custom__button'>View More</button>
+
+      </div>
+      <div className='app__gallery-images'>
+        <div className='app__gallery-images_container' ref={scrollRef}>
+          {galleryImages .map((image,index) =>(
+            <div className='app__gallery-images_card flex__center' key={'gallery_image-${index + 1}'}>
+              <img src={image} alt="gallery" />
+              <BsInstagram className='gallery__image-icon'/>
+
+            </div>
+          ))}
+        </div>
+        <div className='app__gallery-images_arrow'>
+          <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')}/>
+          <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')}/>
+
+
+        </div>
 
       </div>
     </div>
