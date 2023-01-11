9cc5e85295fed288f1a6024773fe74cdeb5286fe
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-01-11 18:37:55 +0200

    added the find us section

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { SubHeading} from '../../components';
+import { images } from '../../constants';
+
+import './Findus.css';
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg__wrapper section__padding' id='Contact'>
+    <div className='app__wrapper_info'>
+    <SubHeading title="Contacts" />
+    <h1 className='headtext__cormorant' style={{ marginBottom: '3rem' }}>Find Us</h1>
+    <div className='app__wrapper-content'>
+      <p className='P-opensans'>Close to Steers in Zomba town. The name of the bulding is the best of all. And at the second floor of that bulding the first room that's it.</p>
+      
+
+    </div>
+    
+
+    </div>
+   <div className='app__wrapper_img'>
+    <img src={images.findus} alt="findus" />
+
+   </div>
+   
   </div>
 );
 
