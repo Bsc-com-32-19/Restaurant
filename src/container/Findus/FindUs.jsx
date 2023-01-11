a6292e12c69a47c6e60cddd00f6a8d27054a6905
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-01-11 19:00:53 +0200

    FindUs sction done

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -10,9 +10,11 @@ const FindUs = () => (
     <SubHeading title="Contacts" />
     <h1 className='headtext__cormorant' style={{ marginBottom: '3rem' }}>Find Us</h1>
     <div className='app__wrapper-content'>
-      <p className='P-opensans'>Close to Steers in Zomba town. The name of the bulding is the best of all. And at the second floor of that bulding the first room that's it.</p>
-      
-
+      <p className='P-opensans'>Close to Steers in Zomba town . The name of  "THE BEST OF ALL" . And at the second floor of that bulding the first room that's it.</p>
+      <p className='P-cormorant' style={{ color: '#DCCA87',margin: '2rem 0'}}>Opening Hours</p>
+      <p className='P-opensans'>Mon - Fri: 08:00am - 02:00 am</p>
+      <p className='P-opensans'>Sat - Sun: 09:00am - 03:00am</p>
+       <button className='custom__button' style={{ marginTop: '2rem'}}>Visit Us</button>
     </div>
     
 
