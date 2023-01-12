db30fad1ad775ff47d56a7214dcef2762e19a2fa
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-01-12 15:36:33 +0200

    added newsletter part

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -4,8 +4,17 @@ import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title="Newsletter"/>
+      <h1 className='headtext__cormorant'>Subcribe To Our Newsletter</h1>
+      <p className='P__opensans'>And never miss lates Updates!</p>
+    </div>
+    <div className='app__newsletter-input flex__center'>
+      <input type="email" placeholder="Enter your e-mail address"/>
+      <button className='custom__button'>Subscribe</button>
+
+    </div>
   </div>
 );
 
