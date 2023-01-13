d7eae845788ec596d0de2fe424e843e9710f9ba1
bsc-com-32-19 <bsc-com-32-19@unima.ac.mw>
2023-01-13 14:25:42 +0200

    added working hours section

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -10,15 +10,15 @@ const Footer = () => (
   <div className='app__footer section__pading'>
     <FooterOverlay/>
     <Newsletter/>
+    
     <div className='app__footer-links'>
       <div className='app__footer-links_contact'>
         <h1 className='app__footer-headtext'>Contact Us</h1>
         <p className='P__opensans'>Post Office Box 228, Zomba,Malawi.</p>
         <p className='P__opensans'>+265994149258</p>
         <p className='P__opensans'>+265888234314</p>
-
-
       </div>
+
       <div className='app__footer-links_logo'>
         <img src={images.gericht} alt="footer_logo" />
         <p className='P__opensans'>"The best way to find yourself is to loose yourself in the service of others."</p>
@@ -27,12 +27,16 @@ const Footer = () => (
           <FiFacebook/>
           <FiTwitter/>
           <FiInstagram/>
-
-
         </div>
-
       </div>
+
       <div className='app__footer-links_work'>
+      <h1 className='app__footer-headtext'>Working Hours</h1>
+        <p className='P__opensans'>Monday-Friday:</p>
+        <p className='P__opensans'>08:00am - 02:00 am</p>
+        <p className='P__opensans'>Saturday-Sunday:</p>
+        <p className='P__opensans'>09:00am - 03:00am</p>
+
 
       </div>
 
